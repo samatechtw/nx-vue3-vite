a50c43cd471eea7e34de05338bbe76fc8c4d5593
SamaTech <samatechtw@gmail.com>
2021-07-27 10:46:11 +0800

    initial commit

---
+++ e2e/vue3-vite-e2e/tests/vue3-vite.spec.ts
@@ -0,0 +1,44 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+describe('vue3-vite e2e', () => {
+  it('should create vue3-vite', async () => {
+    const plugin = uniq('vue3-vite');
+    ensureNxProject('@nx-vue3-vite/vue3-vite', 'dist/packages/vue3-vite');
+    await runNxCommandAsync(
+      `generate @nx-vue3-vite/vue3-vite:vue3-vite ${plugin}`
+    );
+
+    const result = await runNxCommandAsync(`build ${plugin}`);
+    expect(result.stdout).toContain('Executor ran');
+  }, 120000);
+
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const plugin = uniq('vue3-vite');
+      ensureNxProject('@nx-vue3-vite/vue3-vite', 'dist/packages/vue3-vite');
+      await runNxCommandAsync(
+        `generate @nx-vue3-vite/vue3-vite:vue3-vite ${plugin} --directory subdir`
+      );
+      expect(() =>
+        checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)
+      ).not.toThrow();
+    }, 120000);
+  });
+
+  describe('--tags', () => {
+    it('should add tags to nx.json', async () => {
+      const plugin = uniq('vue3-vite');
+      ensureNxProject('@nx-vue3-vite/vue3-vite', 'dist/packages/vue3-vite');
+      await runNxCommandAsync(
+        `generate @nx-vue3-vite/vue3-vite:vue3-vite ${plugin} --tags e2etag,e2ePackage`
+      );
+      const nxJson = readJson('nx.json');
+      expect(nxJson.projects[plugin].tags).toEqual(['e2etag', 'e2ePackage']);
+    }, 120000);
+  });
+});
---
+++ packages/vue3-vite/README.md
@@ -0,0 +1,7 @@
+# vue3-vite
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test vue3-vite` to execute the unit tests via [Jest](https://jestjs.io).
