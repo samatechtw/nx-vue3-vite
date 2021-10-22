1bef889e742f7744693a5a731d5da597effefbdc
SamaTech <samatechtw@gmail.com>
2021-10-22 23:42:13 +0800

    chore: update packages

--- packages/vue3-vite/src/defaults.ts
+++ packages/vue3-vite/src/defaults.ts
@@ -6,50 +6,47 @@ export const recommendedExtensions = [
 ];
 
 // dependency versions
-export const vueVersion = '^3.2.11';
+export const vueVersion = '^3.2.20';
 export const vueI18nVersion = '^9.1.6';
-export const vueRouterVersion = '^4.0.11';
-export const dateFnsVersion = '^2.23.0';
+export const vueRouterVersion = '^4.0.12';
+export const dateFnsVersion = '^2.25.0';
 export const fetchApiVersion = '^0.5.0';
 export const classTransformerVersion = '^0.4.0';
 
 // devDependency versions
-export const viteVersion = '^2.5.7';
+export const viteVersion = '^2.6.10';
 export const eslintVersion = '^7.32.0';
-export const eslintI18nVersion = '^0.12.0';
-export const eslintImportVersion = '^2.24.0';
-export const eslintVueVersion = '^7.16.0';
-export const eslintVueTypescript = '^7.0.0';
-export const vueEslintParser = '^7.11.0';
+export const eslintI18nVersion = '^0.15.0';
+export const eslintImportVersion = '^2.25.2';
+export const eslintVueVersion = '^7.20.0';
+export const eslintVueTypescript = '^8.0.0';
+export const vueEslintParser = '^8.0.0';
 export const chalkVersion = '^4.1.2';
-export const convertSourceMapVersion = '^1.8.0';
-export const extractCSSVersion = '^0.4.4';
-export const sourceMapVersion = '^0.7.3';
-export const vuePluginVersion = '^1.6.2';
+export const vuePluginVersion = '^1.9.3';
 // TODO -- add or remove once it's out of alpha or vite-jest is used
 // Note -- babel stuff shouldn't be needed with vite-jest
 // https://github.com/samatechtw/nx-vue3-vite/issues/14
 // export const vue3JestVersion = '^27.0.0-alpha.2';
-export const babelJestVersion = '^27.0.6';
-export const babelCommonjsVersion = '^7.15.0';
-export const babelCoreVersion = '^7.15.0';
-export const babelPresetEnvVersion = '^7.15.0';
-export const postcssVersion = '^8.3.6';
+export const babelJestVersion = '^27.3.1';
+export const babelCommonjsVersion = '^7.15.4';
+export const babelCoreVersion = '^7.15.8';
+export const babelPresetEnvVersion = '^7.15.8';
+export const postcssVersion = '^8.3.11';
 export const postcssBasicsVersion = '^0.4.2';
 export const compilerSfcVersion = vueVersion;
-export const stylelintVersion = '^13.13.1';
-export const stylelintConfigVersion = '^22.0.0';
+export const stylelintVersion = '^14.0.0';
+export const stylelintConfigVersion = '^23.0.0';
 export const tslibVersion = '^2.3.1';
 export const tsConfigVersion = '^7.0.0';
-export const unpluginVueComponentsVersion = '^0.15.1';
+export const unpluginVueComponentsVersion = '^0.16.0';
 export const viteImagesVersion = '^0.6.1';
 
 // Testing dependencies
 export const cypressVersion = '^8.4.0';
 export const cypressVueVersion = '^3.0.3';
 export const cypressCodeCoverageVersion = '^3.9.11';
-export const cypressViteVersion = '^2.1.0';
-export const eslintCypressVersion = '^2.11.3';
+export const cypressViteVersion = '^2.2.0';
+export const eslintCypressVersion = '^2.12.1';
 
 export const CypressDevDependencies = {
   cypress: cypressVersion,
