4702a9dd415c512bb45f1c9950f2904b0a496a4a
abemscac <abe.msc.ac@gmail.com>
2022-12-28 14:23:44 +0800

    feat: lib generator add path to tsconfig.base.json #35

--- e2e/vue3-vite-e2e/tests/library.spec.ts
+++ e2e/vue3-vite-e2e/tests/library.spec.ts
@@ -1,5 +1,6 @@
 import {
   checkFilesExist,
+  readJson,
   runNxCommandAsync,
   uniq,
 } from '@nrwl/nx-plugin/testing';
@@ -8,10 +9,6 @@ jest.setTimeout(60000);
 
 describe('library e2e', () => {
   it('should create library', async () => {
-    // Create app
-    const app = uniq('vue3-vite');
-    await runNxCommandAsync(`generate nx-vue3-vite:app ${app}`);
-
     // Create library
     const library = uniq('library');
     await runNxCommandAsync(`generate nx-vue3-vite:library ${library}`);
@@ -27,10 +24,6 @@ describe('library e2e', () => {
   });
 
   it('should pass lint check', async () => {
-    // Create app
-    const app = uniq('vue3-vite');
-    await runNxCommandAsync(`generate nx-vue3-vite:app ${app}`);
-
     // Create library
     const library = uniq('library');
     await runNxCommandAsync(`generate nx-vue3-vite:library ${library}`);
@@ -38,5 +31,21 @@ describe('library e2e', () => {
     // Lint
     const lintResult = await runNxCommandAsync(`lint ${library}`);
     expect(lintResult.stdout).toContain('All files pass linting.');
+  });
+
+  it('should add path to `tsconfig.base.json`', async () => {
+    // Create library
+    const library = uniq('some/random/library');
+    await runNxCommandAsync(`generate nx-vue3-vite:library ${library}`);
+
+    // Evaluate key and path
+    const { npmScope } = readJson('nx.json');
+    const key = `@${npmScope}/${library}`;
+    const path = `libs/${library}/src/index.ts`;
+
+    // Check if path is added to `tsConfigBaseJson.compilerOptions.paths` correctly
+    const tsConfigBaseJson = readJson(`tsconfig.base.json`);
+    const { paths } = tsConfigBaseJson.compilerOptions;
+    expect(paths[key]).toEqual([path]);
   });
 });
--- packages/vue3-vite/src/generators/library/generator.spec.ts
+++ packages/vue3-vite/src/generators/library/generator.spec.ts
@@ -1,4 +1,4 @@
-import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+import { Tree, readProjectConfiguration, readJson } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import generator from './generator';
 import { LibraryGeneratorSchema } from './schema';
@@ -9,10 +9,11 @@ describe('vue3-vite library generator', () => {
 
   beforeEach(async () => {
     appTree = createTreeWithEmptyWorkspace();
-    await generator(appTree, options);
   });
 
   it('should run successfully', async () => {
+    await generator(appTree, options);
+
     const config = readProjectConfiguration(appTree, 'test');
     expect(config).toBeDefined();
 
@@ -22,5 +23,54 @@ describe('vue3-vite library generator', () => {
       'lint',
       'test',
     ]);
+  });
+
+  describe('updates `tsconfig.base.json`', () => {
+    it('should create tsconfig.base.json if it does not exist', async () => {
+      // Remove the default `tsconfig.base.json` before generation
+      appTree.delete('tsconfig.base.json');
+
+      // Run generator
+      await generator(appTree, options);
+
+      // Check if `tsconfig.base.json` is generated correctly.
+      // `paths` property will be verified in the test case below.
+      const tsConfigBaseJson = readJson(appTree, 'tsconfig.base.json');
+      expect(tsConfigBaseJson).toMatchObject({
+        compileOnSave: false,
+        compilerOptions: {
+          rootDir: '.',
+          sourceMap: true,
+          declaration: false,
+          moduleResolution: 'node',
+          emitDecoratorMetadata: true,
+          experimentalDecorators: true,
+          importHelpers: true,
+          resolveJsonModule: true,
+          target: 'es2015',
+          module: 'esnext',
+          lib: ['es2017', 'dom'],
+          skipLibCheck: true,
+          skipDefaultLibCheck: true,
+          baseUrl: '.',
+        },
+        exclude: ['node_modules', 'tmp'],
+      });
+    });
+
+    it('should add path to `tsconfigBaseJson.compilerOptions.paths`', async () => {
+      // Run generator
+      await generator(appTree, options);
+
+      // Evaluate key and path
+      const { npmScope } = readJson(appTree, 'nx.json');
+      const key = `@${npmScope}/${options.name}`;
+      const path = `libs/${options.name}/src/index.ts`;
+
+      // Check if path is added to `tsconfigBaseJson.compilerOptions.paths` correctly
+      const tsConfigBaseJson = readJson(appTree, 'tsconfig.base.json');
+      const { paths } = tsConfigBaseJson.compilerOptions;
+      expect(paths[key]).toEqual([path]);
+    });
   });
 });
