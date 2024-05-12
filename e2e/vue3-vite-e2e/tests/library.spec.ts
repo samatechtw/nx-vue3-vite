01233cec78fed2f85ab1cdaa0153dfb88aa9da50
Sam Pullman <sampullman@gmail.com>
2024-05-12 15:04:01 +0800

    fix: adjustments for e2e tests

--- e2e/vue3-vite-e2e/tests/library.spec.ts
+++ e2e/vue3-vite-e2e/tests/library.spec.ts
@@ -16,106 +16,108 @@ jest.setTimeout(60000);
 describe('library e2e', () => {
   let proj: string;
 
-  beforeAll(() => {
-    proj = uniq('vue3-vite');
-    ensureNxProject('nx-vue3-vite', 'dist/packages/vue3-vite', proj);
-  });
-
   afterAll(() => {
     cleanup(proj);
   });
 
-  it('should create library and build', async () => {
-    // Create library
-    const library = uniq('library');
-    await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
-
-    // Check files exist
-    checkFilesExist(proj, [
-      `libs/${library}/postcss.config.js`,
-      `libs/${library}/project.json`,
-      `libs/${library}/vite.config.ts`,
-      `libs/${library}/src/index.ts`,
-      `libs/${library}/src/lib/MyWidget.vue`,
-      `libs/${library}/src/lib/MyWidget.spec.ts`,
-    ]);
-
-    // Build library
-    const result = await runNxCommandAsync(proj, `build ${library}`);
-    expect(result.stdout).toContain('Build complete');
-  });
-
-  it('should make a copy of `package.json` to `dist` if it exists', async () => {
-    // Create library
-    const library = uniq('library');
-    await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
-
-    // Create `package.json`
-    const stringifiedPackageJson = JSON.stringify({
-      name: library,
-      version: '0.0.1',
+  describe('basic library check', () => {
+    beforeAll(() => {
+      proj = uniq('vue3-vite');
+      ensureNxProject('nx-vue3-vite', 'dist/packages/vue3-vite', proj);
     });
-    updateFile(
-      'package.json',
-      stringifiedPackageJson,
-      `${proj}/libs/${library}`,
-    );
-    checkFilesExist(proj, [`libs/${library}/package.json`]);
 
-    // Build library
-    const result = await runNxCommandAsync(proj, `build ${library}`);
-    expect(result.stdout).toContain('Build complete');
+    it('should create library and build', async () => {
+      // Create library
+      const library = uniq('library');
+      await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
 
-    // Verify `package.json` is copied
-    const copiedPackageJson = readJson(
-      proj,
-      `dist/libs/${library}/package.json`,
-    );
-    expect(JSON.stringify(copiedPackageJson)).toEqual(stringifiedPackageJson);
-  });
+      // Check files exist
+      checkFilesExist(proj, [
+        `libs/${library}/postcss.config.js`,
+        `libs/${library}/project.json`,
+        `libs/${library}/vite.config.ts`,
+        `libs/${library}/src/index.ts`,
+        `libs/${library}/src/lib/MyWidget.vue`,
+        `libs/${library}/src/lib/MyWidget.spec.ts`,
+      ]);
 
-  it('should not make a copy of `package.json` to `dist` if it does not exist', async () => {
-    // Create library
-    const library = uniq('library');
-    await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
+      // Build library
+      const result = await runNxCommandAsync(proj, `build ${library}`);
+      expect(result.stdout).toContain('Build complete');
+    });
 
-    // Build library
-    const result = await runNxCommandAsync(proj, `build ${library}`);
-    expect(result.stdout).toContain('Build complete');
+    it('should make a copy of `package.json` to `dist` if it exists', async () => {
+      // Create library
+      const library = uniq('library');
+      await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
 
-    // Verify `package.json` does not exist in library folder
-    const packageJsonInLibrary = `${proj}/libs/${library}/package.json`;
-    expect(exists(packageJsonInLibrary)).toEqual(false);
+      // Create `package.json`
+      const stringifiedPackageJson = JSON.stringify({
+        name: library,
+        version: '0.0.1',
+      });
+      updateFile(
+        'package.json',
+        stringifiedPackageJson,
+        `${proj}/libs/${library}`,
+      );
+      checkFilesExist(proj, [`libs/${library}/package.json`]);
 
-    // Verify `package.json` does not exist in `dist` folder
-    const packageJsonInDist = `${proj}/dist/libs/${library}/package.json`;
-    expect(exists(packageJsonInDist)).toEqual(false);
-  });
+      // Build library
+      const result = await runNxCommandAsync(proj, `build ${library}`);
+      expect(result.stdout).toContain('Build complete');
 
-  it('should pass lint check', async () => {
-    // Create library
-    const library = uniq('library');
-    await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
+      // Verify `package.json` is copied
+      const copiedPackageJson = readJson(
+        proj,
+        `dist/libs/${library}/package.json`,
+      );
+      expect(JSON.stringify(copiedPackageJson)).toEqual(stringifiedPackageJson);
+    });
 
-    // Lint
-    const lintResult = await runNxCommandAsync(proj, `lint ${library}`);
-    expect(lintResult.stdout).toContain('All files pass linting.');
-  });
+    it('should not make a copy of `package.json` to `dist` if it does not exist', async () => {
+      // Create library
+      const library = uniq('library');
+      await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
 
-  it('should add path to `tsconfig.base.json`', async () => {
-    // Create library
-    const library = uniq('some/random/library');
-    await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
+      // Build library
+      const result = await runNxCommandAsync(proj, `build ${library}`);
+      expect(result.stdout).toContain('Build complete');
 
-    // Evaluate key and path
-    const { npmScope } = readJson(proj, 'nx.json');
-    const key = `@${npmScope}/${library}`;
-    const path = `libs/${library}/src/index.ts`;
+      // Verify `package.json` does not exist in library folder
+      const packageJsonInLibrary = `${proj}/libs/${library}/package.json`;
+      expect(exists(packageJsonInLibrary)).toEqual(false);
 
-    // Check if path is added to `tsConfigBaseJson.compilerOptions.paths` correctly
-    const tsConfigBaseJson = readJson(proj, 'tsconfig.base.json');
-    const { paths } = tsConfigBaseJson.compilerOptions;
-    expect(paths[key]).toEqual([path]);
+      // Verify `package.json` does not exist in `dist` folder
+      const packageJsonInDist = `${proj}/dist/libs/${library}/package.json`;
+      expect(exists(packageJsonInDist)).toEqual(false);
+    });
+
+    it('should pass lint check', async () => {
+      // Create library
+      const library = uniq('library');
+      await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
+
+      // Lint
+      const lintResult = await runNxCommandAsync(proj, `lint ${library}`);
+      expect(lintResult.stdout).toContain('All files pass linting.');
+    });
+
+    it('should add path to `tsconfig.base.json`', async () => {
+      // Create library
+      const library = uniq('some/random/library');
+      await runNxCommandAsync(proj, `generate nx-vue3-vite:library ${library}`);
+
+      // Evaluate key and path
+      const { npmScope } = readJson(proj, 'nx.json');
+      const key = `@${npmScope}/${library}`;
+      const path = `libs/${library}/src/index.ts`;
+
+      // Check if path is added to `tsConfigBaseJson.compilerOptions.paths` correctly
+      const tsConfigBaseJson = readJson(proj, 'tsconfig.base.json');
+      const { paths } = tsConfigBaseJson.compilerOptions;
+      expect(paths[key]).toEqual([path]);
+    });
   });
 
   it('should not overwrite `dependencies` in `package.json`', async () => {
@@ -125,7 +127,7 @@ describe('library e2e', () => {
 
     // Install Vue 2
     const packageName = 'vue';
-    const oldVersion = '^2.7.14';
+    const oldVersion = '^2.7.16';
     await runCommandAsync(
       proj,
       `npm install ${packageName}@${oldVersion} --save`,
@@ -151,7 +153,7 @@ describe('library e2e', () => {
 
     // Install Vite 3
     const packageName = 'vite';
-    const oldVersion = '^3.2.7';
+    const oldVersion = '^3.2.10';
     await runCommandAsync(
       proj,
       `npm install ${packageName}@${oldVersion} --save-dev`,
@@ -171,8 +173,13 @@ describe('library e2e', () => {
   });
 
   describe('--test', () => {
-    describe('lints', () => {
-      it('lints and uses Vitest as testing framework by default', async () => {
+    describe('vitest', () => {
+      beforeAll(() => {
+        proj = uniq('vue3-vite');
+        ensureNxProject('nx-vue3-vite', 'dist/packages/vue3-vite', proj);
+      });
+
+      it.only('lints and uses Vitest as testing framework by default', async () => {
         // Reset project to verify correct dependencies are installed
         proj = uniq('vue3-vite');
         ensureNxProject('nx-vue3-vite', 'dist/packages/vue3-vite', proj);
@@ -205,6 +212,28 @@ describe('library e2e', () => {
         // Lint
         const lintResult = await runNxCommandAsync(proj, `lint ${library}`);
         expect(lintResult.stdout).toContain('All files pass linting.');
+      });
+
+      it('runs tests with Vitest when `test` equals to "vitest"', async () => {
+        // Create library
+        const library = uniq('lib-test');
+        await runNxCommandAsync(
+          proj,
+          `generate nx-vue3-vite:library ${library} --test vitest`,
+        );
+
+        // Run test
+        const testResult = await runNxCommandAsync(proj, `test ${library}`);
+        expect(testResult.stdout).toContain(
+          `Successfully ran target test for project ${library}`,
+        );
+      });
+    });
+
+    describe('lints jest', () => {
+      beforeAll(() => {
+        proj = uniq('vue3-vite');
+        ensureNxProject('nx-vue3-vite', 'dist/packages/vue3-vite', proj);
       });
 
       it('lints and uses Jest as testing framework when `test` equals to "jest"', async () => {
@@ -241,23 +270,6 @@ describe('library e2e', () => {
         const lintResult = await runNxCommandAsync(proj, `lint ${library}`);
         expect(lintResult.stdout).toContain('All files pass linting.');
       });
-    });
-
-    describe('runs tests', () => {
-      it('runs tests with Vitest when `test` equals to "vitest"', async () => {
-        // Create library
-        const library = uniq('lib-test');
-        await runNxCommandAsync(
-          proj,
-          `generate nx-vue3-vite:library ${library} --test vitest`,
-        );
-
-        // Run test
-        const testResult = await runNxCommandAsync(proj, `test ${library}`);
-        expect(testResult.stdout).toContain(
-          `Successfully ran target test for project ${library}`,
-        );
-      });
 
       it('runs tests with Jest when `test` equals to "jest"', async () => {
         // Create library
@@ -273,26 +285,28 @@ describe('library e2e', () => {
           `Successfully ran target test for project ${library}`,
         );
       });
-    });
 
-    it('should fail when `test` is not a valid value', async () => {
-      // Create app
-      const library = uniq('lib-test');
-      const wrongValue = 'hello';
-      const createAppCommand = `generate nx-vue3-vite:library ${library} --test ${wrongValue}`;
+      it('should fail when `test` is not a valid value', async () => {
+        // Create app
+        const library = uniq('lib-test');
+        const wrongValue = 'hello';
+        const createAppCommand = `generate nx-vue3-vite:library ${library} --test ${wrongValue}`;
 
-      // Expect it to throw an error
-      await expect(runNxCommandAsync(proj, createAppCommand)).rejects.toThrow();
+        // Expect it to throw an error
+        await expect(
+          runNxCommandAsync(proj, createAppCommand),
+        ).rejects.toThrow();
 
-      // Silence error to verify error message, because the error message thrown
-      // by `runNxCommandAsync` looks like `Command failed: {command}`, which is
-      // not detailed enough.
-      const result = await runNxCommandAsync(proj, createAppCommand, {
-        silenceError: true,
+        // Silence error to verify error message, because the error message thrown
+        // by `runNxCommandAsync` looks like `Command failed: {command}`, which is
+        // not detailed enough.
+        const result = await runNxCommandAsync(proj, createAppCommand, {
+          silenceError: true,
+        });
+        expect(result.stdout).toContain(
+          `Property 'test' does not match the schema. '${wrongValue}' should be one of vitest,jest.`,
+        );
       });
-      expect(result.stdout).toContain(
-        `Property 'test' does not match the schema. '${wrongValue}' should be one of vitest,jest.`,
-      );
     });
   });
 });
