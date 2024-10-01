82513c805dd55d102369502db09dce976fa81851
Sam Pullman <sampullman@gmail.com>
2024-10-02 04:56:18 +0800

    refactor: update packages and check in lockfile

--- packages/vue3-vite/src/util/defaults.ts
+++ packages/vue3-vite/src/util/defaults.ts
@@ -3,33 +3,33 @@ export const VSCodeExtensionsFilePath = '.vscode/extensions.json';
 export const recommendedExtensions = ['vue.volar', 'cpylua.language-postcss'];
 
 // dependency versions
-export const vueVersion = '^3.4.27';
-export const vueI18nVersion = '^9.13.1';
-export const vueRouterVersion = '^4.3.2';
+export const vueVersion = '^3.5.10';
+export const vueI18nVersion = '^9.14.1';
+export const vueRouterVersion = '^4.4.5';
 
 // devDependency versions
-export const viteVersion = '^5.2.11';
+export const viteVersion = '^5.4.8';
 export const eslintVersion = '^8.57.0';
-export const vuePluginVersion = '^5.0.4';
+export const vuePluginVersion = '^5.1.4';
 export const babelJestVersion = '^29.7.0';
 export const jestGlobalsVersion = '^29.7.0';
 export const vue3JestVersion = '^29.2.6';
-export const babelPresetEnvVersion = '^7.24.5';
-export const postcssVersion = '^8.4.38';
+export const babelPresetEnvVersion = '^7.25.4';
+export const postcssVersion = '^8.4.47';
 export const postcssBasicsVersion = '^0.7.4';
-export const stylelintVersion = '^16.5.0';
-export const stylelintConfigVersion = '^36.0.0';
-export const tslibVersion = '^2.6.2';
+export const stylelintVersion = '^16.9.0';
+export const stylelintConfigVersion = '^36.0.1';
+export const tslibVersion = '^2.7.0';
 export const tsConfigVersion = '^7.0.0';
-export const nrwlJestVersion = '^18.3.4';
-export const typescriptVersion = '~5.4.5';
+export const nrwlJestVersion = '^19.8.3';
+export const typescriptVersion = '~5.6.2';
 
 export const CypressDevDependencies = {
-  cypress: '^13.9.0',
-  '@cypress/vue': '^6.0.0',
-  '@cypress/vite-dev-server': '^5.0.7',
-  '@cypress/code-coverage': '^3.12.39',
-  'eslint-plugin-cypress': '^3.2.0',
+  cypress: '^13.15.0',
+  '@cypress/vue': '^6.0.1',
+  '@cypress/vite-dev-server': '^5.2.0',
+  '@cypress/code-coverage': '^3.13.3',
+  'eslint-plugin-cypress': '^3.5.0',
   '@nx/cypress': '*',
 };
 
@@ -38,9 +38,9 @@ export const VueDevDependencies = {
 };
 
 export const VitestDevDependencies = {
-  '@nx/vite': '^18.3.4',
-  'happy-dom': '^14.10.1',
-  vitest: '^1.6.0',
+  '@nx/vite': '^19.8.3',
+  'happy-dom': '^15.17.4',
+  vitest: '^2.1.1',
 };
 
 export const JestDevDependencies = {
@@ -52,35 +52,35 @@ export const JestDevDependencies = {
 };
 
 export const LintDevDependencies = {
-  '@intlify/eslint-plugin-vue-i18n': '^2.0.0',
+  '@intlify/eslint-plugin-vue-i18n': '^3.0.0',
   '@vue/eslint-config-typescript': '^13.0.0',
   eslint: eslintVersion,
   'eslint-config-prettier': '^9.1.0',
-  'eslint-plugin-import': '^2.29.1',
-  'eslint-plugin-vue': '^9.26.0',
+  'eslint-plugin-import': '^2.30.0',
+  'eslint-plugin-vue': '^9.28.0',
   stylelint: stylelintVersion,
   'stylelint-config-standard': stylelintConfigVersion,
-  '@typescript-eslint/eslint-plugin': '7.8.0',
-  '@typescript-eslint/parser': '7.8.0',
-  'vue-eslint-parser': '^9.4.2',
+  '@typescript-eslint/eslint-plugin': '8.8.0',
+  '@typescript-eslint/parser': '8.8.0',
+  'vue-eslint-parser': '^9.4.3',
 };
 
 export const ProjectDependencies = {
   vue: vueVersion,
   'vue-i18n': vueI18nVersion,
   'vue-router': vueRouterVersion,
-  'date-fns': '^3.6.0',
+  'date-fns': '^4.1.0',
 };
 
 export const ProjectDevDependencies = {
   vite: viteVersion,
-  picocolors: '1.0.0',
+  picocolors: '1.1.0',
   'class-transformer': '^0.5.1',
   postcss: postcssVersion,
   '@vitejs/plugin-vue': vuePluginVersion,
-  '@babel/plugin-transform-modules-commonjs': '^7.24.1',
+  '@babel/plugin-transform-modules-commonjs': '^7.24.8',
   '@babel/preset-env': babelPresetEnvVersion,
-  '@babel/core': '^7.23.7',
+  '@babel/core': '^7.25.2',
   '@samatech/postcss-basics': postcssBasicsVersion,
   tslib: tslibVersion,
   tsconfig: tsConfigVersion,
